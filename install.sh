f74d918c47450ce26fa41c7a2b0214cac2e29cdc
Dgame <rswhite4@googlemail.com>
2020-01-01 15:10:03 +0100

    Changed install script to work on every arch distro

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
+distro="unknown"
+
 if [ -f /etc/os-release ]; then
     . /etc/os-release
     os=$NAME
+    distro=$ID_LIKE
 elif type lsb_release >/dev/null 2>&1; then
     os=$(lsb_release -sd)
     os="${os%\"}" # remove suffix "
@@ -14,13 +17,13 @@ else
     os=$(uname -s)
 fi
 
-echo "OS is $os"
+echo "OS is $os and the distribution is $distro"
 
-if [ "$os" = "Manjaro Linux" ]; then
+if [ "$distro" != "arch" ]; then
     yes j | sudo pacman -S ansible --needed && \
     ansible-playbook --ask-become-pass -i hosts linux.yml && \
     yes j | sudo pacman -Syu
 else
-   echo "Unknow OS: $os"
+   echo "$os ($distro) is not supported"
    exit 1
 fi
