1410ceb43d4e3c9e57c193f6d3b9ebcfe2feaec4
Dgame <rswhite4@googlemail.com>
2020-08-11 21:30:30 +0200

    Update

--- arch.mk
+++ arch.mk
@@ -3,15 +3,11 @@ PLAYBOOK = ansible-playbook
 INVENTORY = -i hosts
 FLAGS = --ask-become-pass
 
-vagrant: install_ansible #install_yay
-	$(PLAYBOOK) $(INVENTORY) localhost.yml
-check: install_ansible #install_yay
+check: install_ansible
 	$(PLAYBOOK) --check $(FLAGS) $(INVENTORY) localhost.yml
 install: install_ansible
 	$(PLAYBOOK) $(FLAGS) $(INVENTORY) localhost.yml
 install_ansible: update_cache
-	yes | LC_ALL=en_US.UTF-8 sudo pacman -S $(ANSIBLE_PKG) --needed
-install_utilities: update_cache
-	yes | LC_ALL=en_US.UTF-8 sudo pacman -S binutils gcc pkg-config fakeroot --needed
+	yes j | sudo pacman -S $(ANSIBLE_PKG) --needed
 update_cache:
-	yes | LC_ALL=en_US.UTF-8 sudo pacman -Syu --needed
+	yes j | sudo pacman -Syu
