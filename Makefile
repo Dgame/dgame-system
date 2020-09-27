979dbc0a5dce3820a8b6ca7c8073b5a3e4b0bdaf
Dgame <rswhite4@googlemail.com>
2020-09-27 22:59:12 +0200

    First running version

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ check:
 	make -f arch.mk check
 install:
 	make -f arch.mk install
+test:
+	molecule test
--- arch.mk
+++ arch.mk
@@ -7,7 +7,10 @@ check: install_ansible
 	$(PLAYBOOK) --check $(FLAGS) $(INVENTORY) localhost.yml
 install: install_ansible
 	$(PLAYBOOK) $(FLAGS) $(INVENTORY) localhost.yml
-install_ansible: update_cache
+install_ansible: rebuild_mirrors
 	yes j | sudo pacman -S $(ANSIBLE_PKG) --needed
-update_cache:
+rebuild_mirrors:
 	yes j | sudo pacman -Sy
+update:
+	yay --save --nocleanmenu --nodiffmenu --answerclean All --answerdiff All --answeredit All --answerupgrade All
+	yes j | sudo pacman -Syu && yay -Syu
