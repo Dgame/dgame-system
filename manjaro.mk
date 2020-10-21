81b3148e3d0f61bf44a34f1dd88ca2a1ab6e7351
Dgame <rswhite4@googlemail.com>
2020-10-21 21:40:12 +0200

    Removed Makefile targets

--- manjaro.mk
+++ manjaro.mk
@@ -12,9 +12,6 @@ install_ansible: rebuild_mirrors
 	yes j | sudo pacman -S $(ANSIBLE_PKG) --needed
 rebuild_mirrors:
 	yes j | sudo pacman -Sy
-update:
-	yay --save --nocleanmenu --nodiffmenu --answerclean All --answerdiff All --answeredit All --answerupgrade All
-	yes j | sudo pacman -Syu && yay -Syu
 test:
 	molecule test -s $(PLAYBOOK_NAME)
 converge:
