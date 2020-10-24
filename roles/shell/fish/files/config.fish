73e8df56d15021e4ea22bcebde4b1b275a838c39
Dgame <rswhite4@googlemail.com>
2020-10-24 20:16:36 +0200

    Fixed starship config

--- roles/shell/fish/files/config.fish
+++ roles/shell/fish/files/config.fish
@@ -176,3 +176,5 @@ alias gvt 'git verify-tag'
 
 alias gwch 'git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip 'git add -A; git rm (git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
+
+starship init fish | source
