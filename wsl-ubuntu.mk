0f954b8f4864c1107e36db9fbfe7f475a4474416
Dgame <rswhite4@googlemail.com>
2020-11-05 20:20:51 +0100

    WSL: make it clear which distro is used

---
+++ wsl-ubuntu.mk
@@ -0,0 +1,22 @@
+ANSIBLE_PKG = ansible
+PLAYBOOK = ansible-playbook
+PLAYBOOK_NAME = wsl-ubuntu
+INVENTORY = -i hosts
+FLAGS = --ask-become-pass
+
+check: install_ansible
+	$(PLAYBOOK) --check $(FLAGS) $(INVENTORY) $(PLAYBOOK_NAME).yml
+install: install_ansible
+	$(PLAYBOOK) $(FLAGS) $(INVENTORY) $(PLAYBOOK_NAME).yml
+install_ansible: update
+	sudo apt-get -y install $(ANSIBLE_PKG)
+update:
+	sudo apt-get -y update
+upgrade:
+	sudo apt-get -y upgrade && sudo apt-get -y autoremove
+test:
+	molecule test -s $(PLAYBOOK_NAME)
+converge:
+	molecule converge -s $(PLAYBOOK_NAME)
+idempotence:
+	molecule idempotence -s $(PLAYBOOK_NAME)
